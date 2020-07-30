6bae68b2ddf5e2e083ace72d9e1900ec25ef2cfb
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-07-30 19:10:25 -0400

    added more fields to quarterback serializer

--- quarterbacks/serializers.py
+++ quarterbacks/serializers.py
@@ -6,4 +6,8 @@ class QuarterbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quarterback
         fields = ("id", "name", "team", "ints", "fumbs", "rush_atts", "rush_yards",
-                  "rush_tds", "pass_atts", "pass_comps", "pass_yards", "pass_tds", "games")
+                  "rush_tds", "pass_atts", "pass_comps", "comp_perc", "pass_yards",
+                  "pass_tds", "total_tds", "games", "points", "points_per_game",
+                  "pass_per_td", "yards_per_pass", "yards_per_comp", "rush_per_td",
+                  "yards_per_rush", "total_att", "att_per_game", "points_per_att",
+                  "att_per_td", "avg_vor", "starting_vor")
